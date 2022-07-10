1ebe727ca411638b8f8300b344bd4115b2a90bb8
abolfazl-hue <abolfazljdst@gmail.com>
2022-07-10 15:26:10 +0430

    add some styles to the error layout

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="error-layout">
+    <div class="error-layout w-full mt-20 text-center">
         <RouterView />
-        <h1 class="text-3xl dark:text-gray-200">
+        <h1 class="text-3xl mt-10 dark:text-gray-200">
             {{ $t("tryAnother") }}
         </h1>
-        <span class="text-red-500">{{ $t("errorLayout") }}</span>
+        <span class="text-red-500 block">{{ $t("errorLayout") }}</span>
         <router-link to="/" class="underline text-blue-500" title="'homepage'">{{ $t("homeLink") }}</router-link>
     </div>
 </template>
